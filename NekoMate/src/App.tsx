88305fdbb66725da2d94e6ee6b637d93c6ff24c7
23-prabhat <jhaprabhat.anand@gmail.com>
2025-10-02 16:47:54 +0530

    setup completed

---
+++ NekoMate/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     {/* <h1 className='text-3xl text-orange-400 text-center'>Hello Mate</h1> */}
+     
+    </>
+  )
+}
+
+export default App
